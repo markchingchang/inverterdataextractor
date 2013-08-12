d96ef98c08243a222af5de1145c7924d9e4eba7b
ape.anp@gmail.com <ape.anp@gmail.com@5e75be07-9526-fcbf-8a50-d7e868a687c9>
2013-08-12 19:26:07 +0000

    Initial commit

---
+++ trunk/InveterDataExtractor/LogIn.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace InveterDataExtractor
+{
+    /// <summary>
+    /// Interaction logic for LogInPromtWindow.xaml
+    /// </summary>
+    public partial class LogInPromtWindow : Window
+    {
+        private FileStream fs;
+
+        private const string FileName = "ExtractorLogin.np";
+
+        private readonly string _filePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                                            FileName);
+
+
+        public LogInPromtWindow()
+        {
+            if (!File.Exists(_filePath))
+            {
+                File.Create(_filePath);
+            }
+
+
+
+            InitializeComponent();
+            this.Topmost = true;
+
+
+
+
+
+
+
+        }
+
+        
+    }
+}
---
+++ trunk/InveterDataExtractor/MsgHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InveterDataExtractor
+{
+    public class MsgHandler
+    {
+
+        public MsgHandler()
+        {
+            
+
+        }
+
+
+        public string getCmd(int i)
+        {
+
+            Msg msg = new Msg();
+
+            if (i < 5)
+            {
+                msg.Cmd = "True";
+            }
+            else
+            {
+                msg.Cmd = "False";
+            }
+
+
+
+            return msg.Cmd;
+        }
+
+
+
+
+
+
+
+    }
+
+    class Msg : EventArgs
+    {
+
+        public string Cmd { set; get; }
+    }
+
+
+
+
+}
