c4ad1b7f788648227a23ae1295a2d10b16899ca6
mastruppetersen@gmail.com <mastruppetersen@gmail.com@5e75be07-9526-fcbf-8a50-d7e868a687c9>
2013-08-12 20:19:34 +0000

    Initial MainWindow testet

--- trunk/InveterDataExtractor/MySqlHandler.cs
+++ trunk/InveterDataExtractor/MySqlHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,6 +35,5 @@ namespace InveterDataExtractor
 
 
 
-
     }
-}
+}*/
