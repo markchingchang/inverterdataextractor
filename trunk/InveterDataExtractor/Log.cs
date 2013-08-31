025027716a7a9bcc176ccfd88a20d0f7b3fef396
ape.anp@gmail.com <ape.anp@gmail.com@5e75be07-9526-fcbf-8a50-d7e868a687c9>
2013-08-31 11:06:50 +0000

    Test for jenkins

---
+++ trunk/InveterDataExtractor/Log.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InveterDataExtractor
+{
+    public enum LogCtrl
+    {
+        Exists,
+        Created,
+        Error,
+        Info,
+        TX,
+        RX,
+        WriteError,
+        OK,
+    }
+    
+    public class Log
+    {
+
+        
+
+
+
+
+        private readonly string _folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private readonly string _appName;
+        private string _fullPath;
+
+        public Log()
+        {
+            
+        }
+
+
+        public Log(string appName)
+        {
+            _appName = appName;
+
+            CreateLog(Path.Combine(_folderPath, _appName, "Log"));
+        }
+
+        public LogCtrl CreateLog(string path)
+        {
+
+            System.IO.Directory.CreateDirectory(path);
+            
+            _fullPath = path + "\\" + GetDate() + ".txt";
+
+            if(File.Exists(_fullPath))
+                return LogCtrl.Exists;
+
+            File.Create(_fullPath);
+
+            if (File.Exists(_fullPath))
+                return LogCtrl.Created;
+
+
+            return LogCtrl.Error;
+        }
+
+        public bool FileExists()
+        {
+            return File.Exists(_fullPath);
+        }
+
+        static public string GetDate()
+        {
+            return DateTime.Now.Date.ToString().Substring(0,10);
+        }
+
+        public LogCtrl WriteToLog(string logMsg, LogCtrl ctrl)
+        {
+            if (FileExists())
+            {
+                FileStream fs = File.Open(_fullPath, FileMode.Append, FileAccess.Write, FileShare.Read);
+
+                if (fs.CanWrite)
+                {
+
+                    logMsg = GetDate() + " " + getTime() + "\t[" + ctrl.ToString() + "]\t" + logMsg + "\r\n";
+                    
+                    byte[] info = new UTF8Encoding(true).GetBytes(logMsg);
+                    fs.Write(info, 0, info.Length);
+                    fs.Close();
+                }
+
+
+                return LogCtrl.OK;
+            }
+
+            return LogCtrl.WriteError;
+        }
+
+
+        private string getTime()
+        {
+            
+            return DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString() + ":" + DateTime.Now.Second.ToString() + "." +
+                   DateTime.Now.Millisecond.ToString();
+        }
+
+
+       
+
+    }
+
+
+
+
+}
